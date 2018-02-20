3cefd045f9395baa537b789b661746fca4af0b87
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-02-21 09:13:37 +1100

    Add admin elevation

--- DeployLocalWempUI.ps1
+++ DeployLocalWempUI.ps1
@@ -1,3 +1,47 @@
+#############################################
+# check admin role
+#############################################
+
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   # We are running "as Administrator" - so change the title and background color to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+   clear-host
+   }
+else
+   {
+   # We are not running "as Administrator" - so relaunch as administrator
+   
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+   
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+   
+   # Exit from the current, unelevated, process
+   exit
+}
+
+
+#############################################
+# start deployment
+#############################################
+
 Import-Module WebAdministration 
 
 $error.clear()
@@ -100,3 +144,8 @@ catch
 {
     Write-Host "Error!"
 }
+
+#############################################
+# Finished
+#############################################
+Read-Host -Prompt "Press any key to exit"
