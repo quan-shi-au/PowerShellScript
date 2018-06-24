a641f3e5b6feaff0f91e1362cd7d42f505ae479f
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-06-25 09:08:38 +1000

    no message

---
+++ EventNotificationDeploy.ps1
@@ -0,0 +1,33 @@
+$TaskName = "ConsoleApplication1"
+$SourcePath = "D:\GitHub\ConsoleApplication1\ConsoleApplication1\bin\Debug"
+$TargetPath = "D:\ScheduledTasks\ConsoleApplication1"
+
+# copy files from source to target
+if(Test-Path $TargetPath)
+{
+    Remove-Item $TargetPath -Recurse -Force
+}
+
+New-Item -ItemType Directory -Force -Path $TargetPath
+
+Copy-Item -Path $SourcePath\* -Destination $TargetPath -recurse -Force
+
+# unregister task if exists already
+$task = Get-ScheduledTask | Where-Object TaskName -EQ $TaskName
+
+if ($task)
+{
+    $taskName = $task.TaskName
+
+    Write-Output "Unregister task: $taskName" 
+
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+}
+
+# register scheduled task
+
+$action = New-ScheduledTaskAction -Execute $TargetPath\'ConsoleApplication1.exe' -WorkingDirectory $TargetPath
+$trigger =  New-ScheduledTaskTrigger -Daily -At 9am
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $TaskName -Description $TaskName
+
+
